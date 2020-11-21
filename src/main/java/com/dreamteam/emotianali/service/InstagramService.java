757542e560ed91c84cb9f37733579860040b6363
TomSuworof <rc1995king@gmail.com>
2020-11-21 18:42:58 +0300

    -feature(): coming

--- src/main/java/com/dreamteam/emotianali/service/InstagramService.java
+++ src/main/java/com/dreamteam/emotianali/service/InstagramService.java
@@ -24,36 +24,36 @@ public class InstagramService {
 
     public String getToken(String code) {
         final RestTemplate restTemplate = new RestTemplate();
-        String url = "https://api.instagram.com/oauth/access_token?";
+        String url = "https://api.instagram.com/oauth/access_token";
 //                "client_id=" + INSTAGRAM_ID +
 //                "client_secret=" + INSTAGRAM_CLIENT_SECRET +
 //                "grant_type=authorization_code" +
 //                "redirect_uri=" + INSTAGRAM_REDIRECT_URI +
 //                "code=" + code;
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-//        Map<String, String> vars = new HashMap<>();
-//        vars.put("code", code);
-//        vars.put("redirect_uri", INSTAGRAM_REDIRECT_URI);
-//        vars.put("grant_type", "authorization_code");
-//        vars.put("client_secret", INSTAGRAM_CLIENT_SECRET);
-//        vars.put("client_id", INSTAGRAM_ID);
-//
-//        HttpEntity<Map<String, String>> entity = new HttpEntity<>(vars, headers);
-//        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
-//
-//        String answer = response.getBody();
-//        System.out.println(answer);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        Map<String, String> vars = new HashMap<>();
+        vars.put("code", code);
+        vars.put("redirect_uri", INSTAGRAM_REDIRECT_URI);
+        vars.put("grant_type", "authorization_code");
+        vars.put("client_secret", INSTAGRAM_CLIENT_SECRET);
+        vars.put("client_id", INSTAGRAM_ID);
 
-//        final String answer = restTemplate.postForObject(url, vars, String.class);
+        HttpEntity<Map<String, String>> entity = new HttpEntity<>(vars, headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+        String answer = response.getBody();
+        System.out.println(answer);
+
+//        final String answer = restTemplate.getForObject(url, vars, String.class);
 //        JsonObject jsonObject = new Gson().fromJson(answer, JsonObject.class);
 //
 //        if (!jsonObject.get("error").getAsString().isEmpty()) {
 //            throw new RuntimeException("Invalid token");
 //        }
 //        return jsonObject.get("access_token").getAsString();
-        return code;
+        return answer;
     }
 
     public List<String> getPosts(String token) {
